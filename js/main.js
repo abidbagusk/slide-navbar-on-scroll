d78d4556a8bc304a5929c54fc3b9a0bd51bcb0a0
abidbagusk <abidbagudkurniawan@gmail.com>
2019-02-04 20:03:56 +0700

    add animation when scroll

--- js/main.js
+++ js/main.js
@@ -7,9 +7,14 @@ function slideDown() {
     var scrollPos = document.documentElement.scrollTop;
     // Call Navbar
     var navbar = document.getElementsByClassName('navbar')[0];
+    // Call Content
+    var content = document.getElementsByClassName('content')[0];
     if (scrollPos > 50) {
         navbar.style.top = '0';
+        content.style.backgroundColor = 'orange';
+        content.style.transition = '1s';
     } else {
         navbar.style.top = '-60px';
+        content.style.backgroundColor = 'purple';
     }
 }
