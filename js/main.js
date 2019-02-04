90218179514c8cc33a29c862030c80b8e75b1d97
abidbagusk <abidbagudkurniawan@gmail.com>
2019-02-04 19:59:32 +0700

    create animation when scroll

---
+++ js/main.js
@@ -0,0 +1,15 @@
+window.onscroll = function () {
+    slideDown()
+};
+
+function slideDown() {
+    // Know the scroll position
+    var scrollPos = document.documentElement.scrollTop;
+    // Call Navbar
+    var navbar = document.getElementsByClassName('navbar')[0];
+    if (scrollPos > 50) {
+        navbar.style.top = '0';
+    } else {
+        navbar.style.top = '-50px';
+    }
+}
